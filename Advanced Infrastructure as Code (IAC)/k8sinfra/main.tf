912000aba7f9de2fb1a8666126da07aa933a07d2
devopskroy <109326965+devopskroy@users.noreply.github.com>
2023-10-20 12:29:41 +0530

    Update main.tf

--- Advanced Infrastructure as Code (IAC)/k8sinfra/main.tf
+++ Advanced Infrastructure as Code (IAC)/k8sinfra/main.tf
@@ -1,7 +1,20 @@
+resource "aws_s3_bucket" "eks_cluster_backup" {
+  bucket = "eks-cluster-backup-bucket" # Choose a unique name
+  acl    = "private" 
+}
+resource "aws_cloudwatch_log_group" "eks_cluster_logs" {
+  name              = "eks-cluster-logs"
+  retention_in_days = 7 
+} 
+
 resource "aws_eks_cluster" "my_cluster" {
   name     = var.cluster_name
   role_arn = aws_iam_role.eks_cluster_role.arn
   enabled_cluster_log_types = ["api", "audit"]
+  
+  cloudwatch_log_group {
+    name = aws_cloudwatch_log_group.eks_cluster_logs.name
+  }
   vpc_config {
     subnet_ids = [aws_subnet.example1.id, aws_subnet.example2.id]
   }
@@ -26,3 +39,20 @@ resource "aws_eks_node_group" "my_node_group" {
 
   
 }
+
+
+resource "null_resource" "restore_eks_cluster" {
+  triggers = {
+    cluster_name     = aws_eks_cluster.my_eks_cluster.name
+    bucket_name      = aws_s3_bucket.eks_cluster_backup.bucket
+    bucket_prefix    = "backup"
+  }
+
+  provisioner "local-exec" {
+    command = <<EOT
+      aws eks update-cluster-config --name ${aws_eks_cluster.my_eks_cluster.name} --resources-vpc-config subnetIds="${join(",", module.vpc.private_subnets)}"
+    EOT
+  }
+}
+
+
